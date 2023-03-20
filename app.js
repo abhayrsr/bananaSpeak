5514fc3f2f7978eb3020ab1d1a24216d2a87346d
abhay <abhayrajsinghrana123@gmail.com>
2023-03-20 18:45:33 +0530

    added: API
    The API is working fine

---
+++ app.js
@@ -0,0 +1,19 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverUrl + "?" + "text=" + text;
+}
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+})
